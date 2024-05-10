5013f3c8c01392424649389df1799dc821f8d403
Jasvinder Singh <jasvinder4251@gmail.com>
2024-05-11 00:31:35 +0530

    some css updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+
+
 function App() {
   return (
-    <></>
+    <div id="user-input">
+      <Input  title="INITIAL INVESTMENT"/>
+      <Input  title="ANNUAL INVESTMENT"/>
+      <Input  title="ANNUAL INVESTMENT"/>
+      <Input  title="ANNUAL INVESTMENT"/>
+      <div id></div>
+    </div>
   )
 }
 
