3822bdf1df5d687f0676758f87c3f27a21c1d3b3
Jasvinder Singh <jasvinder4251@gmail.com>
2024-05-14 19:58:45 +0530

    useState for input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import Input from "./components/Input";
+import { useState } from "react";
 
 function App() {
+  let [values, setValues] = useState({
+    "initial investment": 10000,
+    "annual investment": 1200,
+    "expected return": 6,
+    "duration": 10,
+  });
 
+  function handleValueChange(field, newValue) {
+    setValues(preValues => {
+      return {
+        ...preValues,
+        [field]:newValue,
+      }
+    })
+  }
 
   return (
-    <>
-      <div id="user-input">
-        <div className="input-group">
-          <Input  title="INITIAL INVESTMENT" val={10000}/>
-          <Input  title="ANNUAL INVESTMENT"  val={1200}/>
-        </div>
-        <div className="input-group">
-          <Input  title="EXPECTED RETURN" val={6}/>
-          <Input  title="DURATION" val={10}/>
-        </div>
-      </div>  
-    </>
+    <div id="user-input">
+      <div className="input-group">
+        <Input  title="initial investment" val={values["initial investment"]} 
+          onValueChange={handleValueChange} />
+        <Input  title="annual investment"  val={values["annual investment"]} 
+          onValueChange={handleValueChange} />
+      </div>
+      <div className="input-group">
+        <Input  title="expected return" val={values["expected return"]} 
+          onValueChange={handleValueChange} />
+        <Input  title="duration" val={values["duration"]} 
+          onValueChange={handleValueChange} />
+      </div>
+    </div>
   )
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,8 @@
 
 
-export default function Input({title, val}) {
+export default function Input({title, val, onValueChange}) {
   return <p>
     <label>{title}</label>
-    <input type="number" onChange={() => {console.log("Jasvinder")}} value={val} required/>
+    <input type="number" onChange={(e) => {onValueChange(title, e.target.value)}} value={val} required/>
   </p>
 }
